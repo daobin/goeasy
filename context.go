c9483ff5f281ed67ff51bc840c8702c3e389bd92
daobin <shouwenlai@foxmail.com>
2023-07-21 16:18:21 +0800

    添加：上下文中断请求

--- context.go
+++ context.go
@@ -14,10 +14,17 @@ type Context struct {
 	handlersIndex int
 }
 
+// reset 重置相关数据
 func (c *Context) reset() {
 	c.handlersIndex = -1
 }
 
+// Abort 中断请求
+func (c *Context) Abort() {
+	c.handlersIndex = internal.AbortHandlersIndex
+}
+
+// Next 执行下一个handler
 func (c *Context) Next() {
 	// 已中止
 	if c.handlersIndex == internal.AbortHandlersIndex {
@@ -33,22 +40,27 @@ func (c *Context) Next() {
 	}
 }
 
+// render 渲染数据
 func (c *Context) render(code int, r render.IRender) {
 	r.Render(code, c.Writer)
 }
 
+// Json 渲染JSON数据
 func (c *Context) Json(code int, data any) {
 	c.render(code, &render.Json{Data: data})
 }
 
+// bindWith 绑定请求数据到指定对象
 func (c *Context) bindWith(target any, b binder.IBinder) error {
 	return b.Bind(target, c.Request)
 }
 
+// BindJson 绑定JSON数据到指定对象
 func (c *Context) BindJson(target any) error {
 	return c.bindWith(target, &binder.Json{})
 }
 
+// BindQuery 绑定Query数据到指定对象
 func (c *Context) BindQuery(target any) error {
 	return c.bindWith(target, &binder.Query{})
 }
